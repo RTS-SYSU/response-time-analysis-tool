7786d8db0a240421a03a35030308e208d94182ad
吃亏是福 <3528697114@qq.com>
2023-07-26 13:57:07 +0800

    update

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+open module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    exports com.example.demo;
+}
